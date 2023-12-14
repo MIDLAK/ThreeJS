eacbface778096686b06537138e84914ab0aba89
Vadim <kalugavadim04@gmail.com>
2023-12-14 16:02:32 +0700

    shadows

--- main.js
+++ main.js
@@ -10,39 +10,41 @@ window.onload = () => {
     //плоскостей отсечения
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
         0.1, 1000)
-    camera.position.z = 5 //камера отодвигается, чтобы не оказаться в объектах
+    //камера отодвигается, чтобы не оказаться в объектах
+    camera.position.z = 500 
+    camera.position.y = 500
 
     //объект рендера
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
     document.body.appendChild(renderer.domElement)
 
-    var light = new THREE.DirectionalLight(0xca2222, 1);
-    light.position.set(50, 50, 22);
-    light.target.position.set(800, 100, 0);
+
+    //общее освещение
+    //var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.3 )
+    //directionalLight.position.set( -300, 1000, 0 )
+    //scene.add( directionalLight )
+
+    //красное направленное освещение (близко)
+    var light = new THREE.SpotLight(0xca2222, 100000)    
+    light.position.set(-300, 300, 300);
+
+    //настройка теней
+    light.castShadow = true
+    light.distance=1000
+    light.receiveShadow = true
+    light.shadow.mapSize.width = 512
+    light.shadow.mapSize.height = 512
+    light.shadow.camera.near = 0.5
+    light.shadow.camera.far = 10000
+
+
     scene.add(light);
 
-    const helper = new THREE.DirectionalLightHelper(light)
+    const helper = new THREE.CameraHelper(light.shadow.camera)
     scene.add(helper)
-    
-    //light.shadow.camera.near = 0.5;       
-    //light.shadow.camera.far = 5000;      
-    //light.shadow.camera.left = -500;
-    //light.shadow.camera.bottom = -500;
-    //light.shadow.camera.right = 500;
-    //light.shadow.camera.top = 500;
-    
-    //var helper = new THREE.CameraHelper( light.shadow.camera );
-    //scene.add(helper);
-
-    //освещение
-    /*
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 7 );
-    directionalLight.position.y = (5 + 5)
-    directionalLight.position.x = -(5 + 5)
-    directionalLight.position.z = -(5 + 5)
-    directionalLight.target.position.set(100, 82, 0)
-    scene.add(directionalLight);*/
 
     //управление камерой
     const controls = new OrbitControls( camera, renderer.domElement );
@@ -50,25 +52,38 @@ window.onload = () => {
     controls.update();
     renderer.render(scene, camera)
 
-    //создание объекта кубика
-    //для понимания точки отсчёта
-    const geometry = new THREE.BoxGeometry(1, 1, 1) //объект
-    const material = new THREE.MeshBasicMaterial({color: 0x7fffd4}) //материал и цвет
+    //создание объекта куба
+    const geometry = new THREE.BoxGeometry(50, 50, 50) //объект
+    const material = new THREE.MeshLambertMaterial({color: 0x808080}) //материал и цвет
     const cube = new THREE.Mesh(geometry, material) //применение к объекту материала
+    cube.position.set(-200, 0, 0);
+    cube.receiveShadow = true
+    cube.costShadow = true
     scene.add(cube)
 
     //пол
-    const geometryPlane = new THREE.PlaneGeometry(1000, 1000) //объект
-    geometryPlane.rotateX(-Math.PI * 0.5)
-    const floor = new THREE.Mesh(geometryPlane, material) //применение к объекту материала
-    scene.add(floor)
+    const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100, 100)
+    const plane = new THREE.Mesh(planeGeometry, new THREE.MeshLambertMaterial())
+    plane.rotateX(-Math.PI / 2)
+    plane.receiveShadow = true
+    plane.costShadow = true
+    scene.add(plane)
 
 
     //загрузка девушки
     const gltLoader = new GLTFLoader()
-    gltLoader.load( '/static/models/matilda/scene.gltf', (gltf) => {
+    gltLoader.load( '/static/models/matilda2/scene.gltf', (gltf) => {
         const model = gltf.scene
         model.position.set(0, 0, 0);
+        
+        //позволяет объекту отбрасывать тени
+        //и принимать их
+        model.traverse((node) => {
+            if (node.isMesh) {
+                node.castShadow = true
+            }
+        })
+
         //model.scale.set(2,2,2);
         scene.add(gltf.scene)
     })
@@ -76,47 +91,38 @@ window.onload = () => {
     //загрузка стола
     gltLoader.load( '/static/models/table/scene.gltf', (gltf) => {
         const model = gltf.scene
-        model.position.set(100, 0, 0);
+        model.position.set(100, -8, 0);
         model.scale.set(20,20,20);
+
+        //позволяет объекту отбрасывать тени
+        //и принимать их
+        model.traverse((node) => {
+            if (node.isMesh) {
+                node.castShadow = true
+                node.receiveShadow = true
+            }
+        })
+
         scene.add(gltf.scene)
     })
 
     //загрузка молока 
     gltLoader.load( '/static/models/milk/scene.gltf', (gltf) => {
         const model = gltf.scene
-        model.position.set(100, 82, 0);
+        model.position.set(100, 74, 0);
+
+        //позволяет объекту отбрасывать тени
+        //и принимать их
+        model.traverse((node) => {
+            if (node.isMesh) {
+                node.castShadow = true
+            }
+        })
         model.scale.set(200,200,200);
         scene.add(gltf.scene)
+
+        light.target.position = model
     })
-
-    /**
-     * Событие для передвижения камеры по сцене
-     */
-    /*window.addEventListener("keydown", (e) => {
-        console.log(e.key)
-        switch (e.key) {
-            case 's': 
-                camera.position.z += 2
-                break
-            case 'w': 
-                camera.position.z -= 2
-                break
-            case 'd': 
-                camera.position.x += 2
-                break
-            case 'a': 
-                camera.position.x -= 2
-                break
-            case 'q':
-                camera.position.y += 2
-                break
-            case 'e':
-                camera.position.y -= 2
-        }
-        //camera.lookAt(0, 0, 0)
-        renderer.render(scene, camera)
-    })*/
-
 
 
     function animate() {
